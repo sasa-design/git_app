8fa5f42294e3c3c05a1fc04c129d0f309221147e
root <root@ip-10-0-0-219.ap-northeast-1.compute.internal>
2021-05-10 06:10:45 +0000

    deploy-2

--- config/unicorn.conf.rb
+++ config/unicorn.conf.rb
@@ -1,7 +1,7 @@
 $worker  = 10
 $timeout = 30
 $app_dir = "/rails_vue_app" #自分のアプリケーション名
-$listen  = File.expand_path 'tmp/sockets/.unicorn.sock', $app_dir
+$listen  = 3000
 $pid     = File.expand_path 'tmp/pids/unicorn.pid', $app_dir
 $std_log = File.expand_path 'log/unicorn.log', $app_dir
 # set config
