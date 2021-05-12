98eaeedd59f30b9478c2a06a0cb3475ea2d5f04e
root <root@ip-10-0-0-219.ap-northeast-1.compute.internal>
2021-05-12 15:33:51 +0000

    deploy-5

--- config/unicorn.conf.rb
+++ config/unicorn.conf.rb
@@ -1,27 +1,30 @@
-$worker  = 10
-$timeout = 30
-$app_dir = "/rails_vue_app" #自分のアプリケーション名
-$listen  = 3000
-$pid     = File.expand_path 'tmp/pids/unicorn.pid', $app_dir
-$std_log = File.expand_path 'log/unicorn.log', $app_dir
-# set config
-worker_processes  $worker
-working_directory $app_dir
-timeout $timeout
-# loading booster
-preload_app true
-# before starting processes
-before_fork do |server, worker|
-  defined?(ActiveRecord::Base) and ActiveRecord::Base.connection.disconnect!
-  old_pid = "#{server.config[:pid]}.oldbin"
-  if old_pid != server.pid
-    begin
-      Process.kill "QUIT", File.read(old_pid).to_i
-    rescue Errno::ENOENT, Errno::ESRCH
+  $worker  = 5
+  $timeout = 30
+  $app_dir = "/rails_vue_app" #自分のアプリケーション名
+  $listen  = File.expand_path 'tmp/sockets/.unicorn.sock', $app_dir
+  $pid     = File.expand_path 'tmp/pids/unicorn.pid', $app_dir
+  $std_log = File.expand_path 'log/unicorn.log', $app_dir
+  # set config
+  worker_processes  $worker
+  working_directory $app_dir
+  stderr_path $std_log
+  stdout_path $std_log
+  timeout $timeout
+  listen  $listen
+  # loading booster
+  preload_app true
+  # before starting processes
+  before_fork do |server, worker|
+    defined?(ActiveRecord::Base) and ActiveRecord::Base.connection.disconnect!
+    old_pid = "#{server.config[:pid]}.oldbin"
+    if old_pid != server.pid
+      begin
+        Process.kill "QUIT", File.read(old_pid).to_i
+      rescue Errno::ENOENT, Errno::ESRCH
+      end
     end
   end
-end
-# after finishing processes
-after_fork do |server, worker|
-  defined?(ActiveRecord::Base) and ActiveRecord::Base.establish_connection
-end
+  # after finishing processes
+  after_fork do |server, worker|
+    defined?(ActiveRecord::Base) and ActiveRecord::Base.establish_connection
+  end
