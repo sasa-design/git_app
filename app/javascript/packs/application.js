de83afe0c65e7e0197a49d60549b55e038dc1e18
root <root@ip-10-0-0-219.ap-northeast-1.compute.internal>
2021-05-17 15:29:45 +0000

    nginx-2

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,13 +3,41 @@
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
 
-import Rails from "@rails/ujs"
-//import Turbolinks from "turbolinks"
-import * as ActiveStorage from "@rails/activestorage"
-import "channels"
+require('@rails/ujs').start();
+require('@rails/activestorage').start();
+require('channels');
+
+import Vue from 'vue'
+import App from '../app.vue'
+import VueRouter from 'vue-router'
+import router from '../router/router'
+import stylesheet from '../stylesheets/application.scss'
+import js from 'materialize-css/dist/js/materialize.min.js'
+import Store from '../store/store.js'
+import icon from 'material-design-icons/iconfont/material-icons.css'
+import qs from 'qs'
 
 
-Rails.start()
-//Turbolinks.start()
-ActiveStorage.start()
+Vue.use(VueRouter);
 
+document.addEventListener('DOMContentLoaded', () => {
+  const app = new Vue({
+    store:Store,
+    qs,
+    js,
+    stylesheet,
+    icon,
+    router,
+    created() {
+      const userString = localStorage.getItem('currentUser');
+      if (userString) {
+        const userData = JSON.parse(userString);
+        this.$store.commit('auth/SET_CURRENT_USER', userData);
+      }
+    },
+    render: h => h(App)
+  }).$mount()
+  document.body.appendChild(app.$el)
+
+  console.log(app)
+})
