5317e4bd3d396bf8fd513b829f9a226cc164725e
root <root@ip-10-0-0-83.ap-northeast-1.compute.internal>
2021-08-19 09:15:27 +0000

    production

--- config/unicorn.conf.rb
+++ config/unicorn.conf.rb
@@ -1,6 +1,6 @@
   $worker  = 5
   $timeout = 30
-  $app_dir = File.expand_path('../../', __FILE__) #appname
+  $app_dir = "/git_app" #appname
   $listen  = File.expand_path 'tmp/sockets/.unicorn.sock', $app_dir
   $pid     = File.expand_path 'tmp/pids/unicorn.pid', $app_dir
   $std_log = File.expand_path 'log/unicorn.log', $app_dir
